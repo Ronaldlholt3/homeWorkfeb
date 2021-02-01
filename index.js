159d702238222f4abebbfa30ddd140a83ddcc7ee
Ronaldlholt3 <76957909+Ronaldlholt3@users.noreply.github.com>
2021-02-01 18:23:09 -0500

    homeWork

---
+++ index.js
@@ -0,0 +1,23 @@
+var arr1 = [1,2,3];
+arr1.push(4);
+
+var arr2 = ["Stimpson", "J", "cat"];
+arr2.push(["happy", "joy"]);
+
+var arr3 = [1,2,3,4]
+arr3.splice(2,2);
+
+var arr4 = [1,2,3,4,5]
+arr4.slice(0,3);
+
+var arr5 = [1,2,3,4,5,6]
+
+var arr6 = [1,2,3,4,5,6,7]
+
+var arr7 = [1,2,3,4,5,6,7,8]
+
+var arr8 = [1,2,3,4,5,6,7,8,9]
+
+var arr9 = [1,2,3,4,5,6,7,8,9,10]
+
+var arr10 = [1,2,3,4,5,6,7,8,9,10,11]
